1dcd8c5c0a5fe01ce5608cecdd0bbbeda535a87c
jittimapis <jittima@myorder.ai>
2026-02-21 13:33:15 +0700

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script src="https://mcp.figma.com/mcp/html-to-design/capture.js" async></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
